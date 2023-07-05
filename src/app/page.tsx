65f95d47f7136af88689f821eeac86fef3184707
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-05 12:19:06 +0300

    feat: import header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <main className="flex min-h-screen px-10"></main>;
+  return (
+    <main className="flex min-h-screen px-10">
+      <Header />
+    </main>
+  );
 }
