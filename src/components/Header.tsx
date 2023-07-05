ec00206fdb3ac726b7c24969079cca640aee6eeb
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-05 12:18:47 +0300

    feat: added react-icons and styled header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
+import { HiOutlineMoon } from "react-icons/hi";
+
 const Header = () => {
   return (
-    <header>
+    <header className="flex justify-between items-center shadow-md">
       <h1>Where in the world?</h1>
-      <span>Dark Mode</span>
+      <div className="flex items-center gap-2">
+        <HiOutlineMoon size={16} />
+        <span>Dark Mode</span>
+      </div>
     </header>
   );
 };
