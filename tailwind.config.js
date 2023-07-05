71d6935b048f437e23824b2619a33e739bbf1db5
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-05 12:13:44 +0300

    added project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+  colors: {
+    "c-white": "hsl(0, 0%, 100%)",
+    "vl-gray": "hsl(0, 0%, 98%)",
+    "dark-gray": "hsl(0, 0%, 52%)",
+    "db-text": "hsl(200, 15%, 8%)",
+    "db-bg": "hsl(209, 23%, 22%)",
+    "db-el": "hsl(207, 26%, 17%)",
   },
   plugins: [],
-}
+};
