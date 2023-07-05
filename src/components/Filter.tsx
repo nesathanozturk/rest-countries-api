c27ea39c76d58b0160a7bef5f4622c73d40d2b86
Neşathan Öztürk <nesathanozturk@Nesathan-MacBook-Pro.local>
2023-07-05 13:28:24 +0300

    style: edited input and slecet

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -3,7 +3,7 @@ import Input from "./Input";
 
 const Filter = () => {
   return (
-    <div className="flex justify-between px-5 md:px-20">
+    <div className="flex gap-10 sm:gap-0 flex-col sm:flex-row justify-between px-5 md:px-20">
       <Input />
       <Select />
     </div>
--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -3,11 +3,11 @@ import { FaAngleDown } from "react-icons/fa";
 const Select = () => {
   return (
     <div className="text-lm-txt">
-      <div className="bg-lm-el p-4 px-5 rounded-md flex gap-8 items-center shadow-sm">
+      <div className="w-44 bg-lm-el p-4 px-5 rounded-md flex justify-between items-center shadow-sm">
         <h1 className="text-xs font-semibold">Filter by Region</h1>
-        <FaAngleDown size={13} />
+        <FaAngleDown size={13} className="cursor-pointer" />
       </div>
-      <div className="hidden bg-lm-el p-4 px-6 rounded-md mt-1 font-semibold text-xs shadow-sm">
+      <div className="hidden w-44 bg-lm-el p-4 px-6 rounded-md mt-1 font-semibold text-xs shadow-sm">
         <ul>
           <li className="mb-2">Africa</li>
           <li className="mb-2">America</li>
